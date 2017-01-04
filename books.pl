458e86d2d1fc7c5957b46c29c85a8c0aea230e08
Jordan Farnworth <jfarnworth@brightbytes.net>
2017-01-04 08:26:56 -0700

    first two challenges

---
+++ books.pl
@@ -0,0 +1,17 @@
+wrote(fountain_head, rand).
+wrote(atlas_shrugged, rand).
+wrote(anthem, rand).
+wrote(win_big, adams).
+wrote(finders_keepers, king).
+wrote(sun_also_rises, hemingway).
+
+
+instrument(guitar, jordan)
+instrument(drums, sean)
+instrument(flute, drew)
+instrument(bells, drew)
+
+
+genre(rock, jordan)
+genre(rock, sean)
+genre(classical, drew)
---
+++ challenge_2.pl
@@ -0,0 +1,18 @@
+# reverse
+reverse_list([],[]).
+reverse_list([Head|Tail], Rerverse):-  naiverev(Tail; ,RevT),  append(RevT,[Head],Rerverse).
+
+# sort
+naive_sort(List,Sorted):-perm(List,Sorted),is_sorted(Sorted).
+is_sorted([]).
+is_sorted)[_]).
+is_sorted([X,Y|T]):-X=<Y,is_sorted([Y|T]).
+
+# find smallest number in liste
+ist_min([L|Ls], Min) :-
+    list_min(Ls, L, Min).
+
+list_min([], Min, Min).
+list_min([L|Ls], Min0, Min) :-
+    Min1 is min(L, Min0),
+    list_min(Ls, Min1, Min).
